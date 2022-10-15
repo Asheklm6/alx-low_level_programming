93542c322af1231da6998a834d1fa4563fbb92f0
Asheklm6 <asheklm6@gmail.com>
2022-10-15 10:19:57 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry
- */
-
-int main(void)
-
-{
-	int num;
-	char letter;
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
-
-	putchar('\n');
-
-	return (0);
-}
+ * main - Entry point to the function
+ * Return: Always 0
+ */																				int main(void)																			{											int n;																				srand(time(0));									n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);						else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n)";						return (0);								}
