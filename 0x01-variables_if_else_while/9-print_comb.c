bec2de48d5b52cd0d16686d9a5c2409bae03370f
Asheklm6 <asheklm6@gmail.com>
2022-10-15 11:58:55 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,25 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point to the function
+ * main - Etry
  * Return: Always 0
- */																				int main(void)																			{											int n;																				srand(time(0));									n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%i is positive\n", n);						else if (n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n)";						return (0);								}
+ */
+
+int main(void)
+
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
