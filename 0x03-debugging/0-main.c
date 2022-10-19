043eeeedcdf2240625646c82a56287230952b992
Asheklm6 <asheklm6@gmail.com>
2022-10-19 15:11:59 -0700

    main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-#endif
+
+/**
+ * main - Entry point to the function
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
