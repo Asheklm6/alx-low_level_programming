16a9ec4889d0ad111c44f4990b0ebe1312c9f8e2
Asheklm6 <asheklm6@gmail.com>
2022-10-20 10:45:10 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,15 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (size > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 			{
-				_putchar(' ');
+				_putchar(35);
 			}
 
-			_putchar(92);
 			_putchar('\n');
 		}
 	}
